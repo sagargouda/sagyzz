6f9da4301047445f4e1ab3de4f65b861c5dc2b4b
sagar <sagarc2580@gmail.com>
2024-02-02 22:14:33 +0530

    feat: implemented search filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,37 +6,68 @@ import Shimmer from "./Shimmer";
 
 export default function Body(){
   const[rests,setRests] = useState([])
+    const [filterRests , setFilterRests] = useState([])
+    const [name,setName] =useState("")
+
 
     useEffect(()=>{
         fetchData()
     },[])
 
+    useEffect(() => {
+        handleSearch();
+    }, [name,handleSearch]);
+
     async function fetchData(){
      const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.019677&lng=76.111056&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
         const json = await response.json();
-     console.log(json)
-      setRests(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+     const ApiData = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+        setRests(ApiData)
+        setFilterRests(ApiData)
     }
 
     if(rests.length === 0){
      return  <Shimmer/>
     }
 
+    // function for finding top rated restaurants
+    function handleTopRated(){
+        const topRated = rests.filter((restz)=>{
+            return restz.info.avgRating > 4.3
+        })
+        setFilterRests(topRated);
+    }
+
+    //  function for searching restaurants
+    function handleSearch(){
+        const searchFilter = rests.filter((rest)=>{
+            return rest.info.name.toLowerCase().includes(name.toLowerCase())
+        })
+            setFilterRests(searchFilter)
+    }
+
+
+    function handleInputChange(e){
+        setName(e.target.value)
+    }
+
     return (
         <>
             <div className="search">
-                {/*top rated restauarants implementation */}
-                <button onClick={()=>{
-                    const topRated = rests.filter((restz)=>{
-                        return restz.info.avgRating > 4.3
-                    })
-                    setRests(topRated);
-                }}>Top rated Button</button>
+                <button onClick={()=> handleTopRated()} className="button" >Top rated Button</button>
+                <div className="searchInput">
+                    <input type="text" placeholder="Search Restaurants Here" id="inp" value={name} onChange={(e) => handleInputChange(e)}/>
+                    <button className="button" onClick={()=>handleSearch()}>Search</button>
+                </div>
             </div>
             <div className="body-container">
                 {
+                    filterRests.length === 0 ? (
+                        <h1 className="not-found">Sorry, we couldn't find any results for "{name}"</h1>
 
-                    rests.map((rest) => {
+                        ) :
+
+                        filterRests.map((rest) => {
                         return <RestCard key={rest.info.id} resData={rest}/>
                     })
 
