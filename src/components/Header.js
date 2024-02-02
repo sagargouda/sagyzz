bdc627eb4f71027b87e09336cf44a5122e1f333c
sagar <sagarc2580@gmail.com>
2024-02-02 14:21:46 +0530

    feat: add header component with styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
+import './Header.css'
 export default function Header(){
     return(
     <div className="header-container">
-
+        <div className="logo-container">
+            <img src={require('./sagyzz.png')} alt="sagyzz-logo" className="logo"/>
+        </div>
+        <div className="nav-container">
+            <ul className="nav-container__list">
+                <li className="nav-container__item">Home</li>
+                <li className="nav-container__item">About</li>
+                <li className="nav-container__item">Contact us</li>
+            </ul>
+        </div>
     </div>
     )
 }
