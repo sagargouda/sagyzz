018f9573213be306ae2a4bf16f03a1f4d4f3d680
sagar <sagarc2580@gmail.com>
2024-02-02 14:32:22 +0530

    chore: update logo , font size

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ export default function Header(){
     return(
     <div className="header-container">
         <div className="logo-container">
-            <img src={require('./sagyzz.png')} alt="sagyzz-logo" className="logo"/>
+            <img src={require('./saglogo.png')} alt="sagyzz-logo" className="logo"/>
         </div>
         <div className="nav-container">
             <ul className="nav-container__list">
