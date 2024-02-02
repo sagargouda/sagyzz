3056fd2c719bd3cfb5463acd882f30c3b8568a6e
sagar <sagarc2580@gmail.com>
2024-02-02 17:41:18 +0530

    feat: implement filter button

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,17 +2,24 @@ import './Body.css';
 import {restaurants} from "./data";
 
 import RestCard from "./RestCard";
+import {useState} from "react";
 
 export default function Body(){
+  const[rests,setRests] = useState(restaurants)
 
     return (
         <>
             <div className="search">
-                <button>Top rated Button</button>
+                <button onClick={()=>{
+                    const top = restaurants.filter((resti)=>{
+                        return resti.rating > 4.8
+                    })
+                    setRests(top);
+                }}>Top rated Button</button>
             </div>
             <div className="body-container">
                 {
-                    restaurants.map((rest) => {
+                    rests.map((rest) => {
                         return <RestCard key={rest.id} resData={rest}/>
                     })
 
