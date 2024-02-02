e57ecdd006a6575c67c9342e56968c551d63983a
sagar <sagarc2580@gmail.com>
2024-02-02 20:13:55 +0530

    feat: add Shimmer UI effect

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -3,20 +3,22 @@ import './Shimmer.css'
 export default function Shimmer(){
     return (
         <div className="shimmer-container">
-            <div className="shimmer"></div>
-            <div className="shimmer"></div>
-            <div className="shimmer"></div>
-            <div className="shimmer"></div>
-            <div className="shimmer"></div>
-            <div className="shimmer"></div>
-            <div className="shimmer"></div>
-            <div className="shimmer"></div>
-            <div className="shimmer"></div>
-            <div className="shimmer"></div>
-            <div className="shimmer"></div>
-            <div className="shimmer"></div>
-            <div className="shimmer"></div>
-            <div className="shimmer"></div>
+                <div className="shimmer"></div>
+                <div className="shimmer"></div>
+                <div className="shimmer"></div>
+                <div className="shimmer"></div>
+                <div className="shimmer"></div>
+                <div className="shimmer"></div>
+                <div className="shimmer"></div>
+                <div className="shimmer"></div>
+                <div className="shimmer"></div>
+                <div className="shimmer"></div>
+                <div className="shimmer"></div>
+                <div className="shimmer"></div>
+                <div className="shimmer"></div>
+                <div className="shimmer"></div>
+                <div className="shimmer"></div>
+
         </div>
     )
 }
