05187a931d33a90ac520a00b9d62ff474316ffe3
sagar <sagarc2580@gmail.com>
2024-02-03 19:08:55 +0530

    feat: added restaurant menu

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,11 +1,13 @@
 import './Error.css';
 import errorImage from './error.png';
+import {useRouteError} from "react-router-dom";
 
 const Error = () => {
+    const err = useRouteError()
     return (
 
             <div className="error-content">
-                <h1 className="error-title">404</h1>
+                <h1 className="error-title">{err.status}</h1>
                 <p className="error-message">Oops! Looks like you've hit a wrong route.</p>
                 <img src={errorImage} alt="error png" className="error-image" />
                 <p className="error-hint">But don't worry, our Sagyzz engineers are on it! Keep hustling.</p>
