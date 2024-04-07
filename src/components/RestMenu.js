60e84923173fc2bd1fcb8f4bbe2fe4ae167326b9
sagar <sagarc2580@gmail.com>
2024-04-07 15:40:00 +0530

    refactor: cors proxy

--- src/components/RestMenu.js
+++ src/components/RestMenu.js
@@ -22,11 +22,11 @@ export default function RestMenu(){
 
 
 
-    console.log(resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards)
+    console.log(resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards)
 
     // filtering out item category among all cards
 
-    const categories = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c)=>{
+    const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c)=>{
         return c.card?.card?.["@type"] ===
             "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     })
--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -9,7 +9,7 @@ function useRestaurantMenu(resId){
     },[])
 
     async function  fetchData(){
-        const response = await fetch(`https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`)
+        const response = await fetch(` https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`)
         const json = await response.json();
         setResInfo(json.data)
     }
